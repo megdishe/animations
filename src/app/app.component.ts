5db5f6cc09edb53ff6bcf71d1457bac9963711e0
megdishemyga <140427058+megdishemyga@users.noreply.github.com>
2024-09-10 12:19:13 +0200

    trigger and state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {NgFor} from "@angular/common";
+import {state, style, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'blue',
+        transform: 'translateX(0px)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'red',
+        transform: 'translateX(100px)'
+      }))])]
+
 })
 export class AppComponent {
-  title = 'untitled';
+  list = ['Milk', 'Sugar', 'Bread'];
+  state = 'normal';
+
+  onDelete(item: any) {
+    const index = this.list.indexOf(item);
+    this.list.splice(index, 1);
+  }
+
+  onAdd(item: any) {
+    this.list.push(item);
+  }
+
 }
